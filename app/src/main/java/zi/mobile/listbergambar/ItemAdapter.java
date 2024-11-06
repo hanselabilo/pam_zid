96bb788bd7fb356d216cda68c12a255b93c83f77
Noval Raihan Ramadhan <Sleepyneko23@gmail.com>
2024-11-06 15:05:40 +0700

    Bayar 50000

--- app/src/main/java/zi/mobile/listbergambar/ItemAdapter.java
+++ app/src/main/java/zi/mobile/listbergambar/ItemAdapter.java
@@ -1,5 +1,6 @@
 package zi.mobile.listbergambar;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
--- app/src/main/java/zi/mobile/listbergambar/MainActivity.java
+++ app/src/main/java/zi/mobile/listbergambar/MainActivity.java
@@ -9,11 +9,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Cache;
+import com.android.volley.Network;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
@@ -28,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private ItemAdapter adapter;
     private List<Item> itemList;
-    public static final String URL = "http://192.168.100.16/getImages.php";
+    public static final String URL = "https://api.mockfly.dev/mocks/02f77d50-dfd4-4910-af93-6e3fb9b9e5ee/getGambar";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,18 +56,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void fetchImages() {
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        RequestQueue requestQueue;
 
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, URL, null,
-                new Response.Listener<JSONArray>() {
+        Cache cache = new DiskBasedCache(getCacheDir());
+
+        Network network = new BasicNetwork(new HurlStack());
+
+        requestQueue = new RequestQueue(cache, network);
+
+        requestQueue.start();
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, URL, null,
+                new Response.Listener<JSONObject>() {
                     @Override
-                    public void onResponse(JSONArray response) {
+                    public void onResponse(JSONObject response) {
                         try {
-                            for (int i = 0; i < response.length(); i++) {
-                                JSONObject jsonObject = response.getJSONObject(i);
-                                String imageUrl = jsonObject.getString("imageUrl");
-                                String name = jsonObject.getString("name");
-
+                            JSONArray data = response.getJSONArray("data");
+                            for (int i = 0; i < data.length(); i++) {
+                                JSONObject current = data.getJSONObject(i);
+                                String name = current.getString("name");
+                                String imageUrl = current.getString("imageUrl");
+                                Log.d("Test", imageUrl);
                                 itemList.add(new Item(imageUrl, name, "Non Elektronik", "Di Ambil", "60 Menit"));
                             }
                             adapter.notifyDataSetChanged();
@@ -70,6 +86,7 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                 },
+
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
@@ -78,6 +95,8 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
-        requestQueue.add(jsonArrayRequest);
+        requestQueue.add(jsonObjectRequest);
     }
 }
+
+
